dbb1f9c600d43b2ebdcd91d04b752645e38d649b
Otto Alexander Robsahm <111810346+Otro1@users.noreply.github.com>
2024-01-31 13:00:24 +0100

    hfhfh

---
+++ src/modules/application/mapApplication.tsx
@@ -0,0 +1,52 @@
+import React, { MutableRefObject, useEffect, useRef, useState } from "react";
+import { Map, View } from "ol";
+import TileLayer from "ol/layer/Tile";
+import { OSM } from "ol/source";
+import { useGeographic } from "ol/proj";
+
+import "./application.css";
+import "ol/ol.css";
+import { KommuneLayerCheckbox } from "../kommune/kommuneLayerCheckbox";
+import { Layer } from "ol/layer";
+
+// eslint-disable-next-line react-hooks/rules-of-hooks
+useGeographic();
+
+const map = new Map({
+  view: new View({ center: [10, 59], zoom: 8 }),
+});
+
+export function MapApplication() {
+  function handleFocusUser(e: React.MouseEvent) {
+    e.preventDefault();
+    navigator.geolocation.getCurrentPosition((pos) => {
+      const { latitude, longitude } = pos.coords;
+      map.getView().animate({
+        center: [longitude, latitude],
+        zoom: 18.5,
+      });
+    });
+  }
+
+  const [layers, setLayers] = useState<Layer[]>([
+    new TileLayer({ source: new OSM() }),
+  ]);
+
+  const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
+  useEffect(() => map.setTarget(mapRef.current), []);
+  useEffect(() => map.setLayers(layers), [layers]);
+  return (
+    <>
+      <header>
+        <h1>Kommune kart</h1>
+      </header>
+      <nav>
+        <a href={"#"} onClick={handleFocusUser}>
+          Focus on me
+        </a>
+        <KommuneLayerCheckbox map={map} setLayers={setLayers} />
+      </nav>
+      <div ref={mapRef}></div>
+    </>
+  );
+}
